67c9085564c41aaa4a9df5d8e69c9c7720ff2c7d
carminegreen <tec.carmineverde@gmail.com>
2022-06-24 12:27:21 +0200

    (feat): Update resolver.py with com resolution functions

--- universal_resolver/resolver.py
+++ universal_resolver/resolver.py
@@ -2,8 +2,7 @@
 
 import logging
 import json
-import re
-from typing import Optional, Pattern, cast
+from typing import Sequence
 
 import aiohttp
 
@@ -17,23 +16,53 @@ from aries_cloudagent.resolver.base import (
 )
 
 LOGGER = logging.getLogger(__name__)
-DEFAULT_ENDPOINT = "https://dev.uniresolver.io"
-
-
-async def _fetch_resolver_props(endpoint: str) -> dict:
-    """Retrieve universal resolver properties."""
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"{endpoint}/1.0/properties/") as resp:
-            if resp.status >= 200 and resp.status < 400:
-                return await resp.json()
-            raise ValueError(await resp.text())
-
-
-async def _get_supported_methods_pattern(endpoint: str) -> Pattern:
-    props = await _fetch_resolver_props(endpoint)
-    return re.compile(
-        r"(?:" + "|".join(driver["http"]["pattern"] for driver in props.values()) + ")"
-    )
+DEFAULT_CONFIGURATION = {
+    "endpoint": "http://64.225.69.15:8080/1.0/identifiers",
+    "methods": [
+        "sov",
+        "abt",
+        "btcr",
+        "erc725",
+        "dom",
+        "stack",
+        "ethr",
+        "web",
+        "v1",
+        "key",
+        "ipid",
+        "jolo",
+        "hacera",
+        "elem",
+        "seraphid",
+        "github",
+        "ccp",
+        "work",
+        "ont",
+        "kilt",
+        "evan",
+        "echo",
+        "factom",
+        "dock",
+        "trust",
+        "io",
+        "bba",
+        "bid",
+        "schema",
+        "ion",
+        "ace",
+        "gatc",
+        "unisot",
+        "icon",
+        "vaa",
+        "cy",
+        "nacl",
+        "sirius",
+        "mpg",
+        "trustbloc",
+        "hcr",
+        "neoid",
+    ],
+}
 
 
 class UniversalResolver(BaseDIDResolver):
@@ -47,35 +76,39 @@ class UniversalResolver(BaseDIDResolver):
 
     async def setup(self, context: InjectionContext):
         """Preform setup, populate supported method list, configuration."""
-        endpoint = (
-            cast(dict, context.settings.get("plugin_config", {}))
-            .get("http_uniresolver", {})
-            .get("endpoint", DEFAULT_ENDPOINT)
-        )
-        await self.configure(endpoint)
+        plugin_conf = context.settings.get("plugin_config", {}).get("http_uniresolver")
 
-    async def configure(
-        self,
-        endpoint: Optional[str] = None,
-        supported_methods_pattern: Optional[Pattern] = None,
-    ):
-        """Do configuration."""
-        self._endpoint = endpoint or DEFAULT_ENDPOINT
-        self._supported_methods_pattern = (
-            supported_methods_pattern
-            or await _get_supported_methods_pattern(self._endpoint)
-        )
+        configuration = DEFAULT_CONFIGURATION
+        if plugin_conf:
+            configuration.update(plugin_conf)
+
+        self.configure(configuration)
+
+    def configure(self, configuration: dict):
+        """Configure this instance of the resolver from configuration dict."""
+        try:
+            self._endpoint = configuration["endpoint"]
+            self._supported_methods = configuration["methods"]
+        except KeyError as err:
+            raise ResolverError(
+                f"Failed to configure {self.__class__.__name__}, "
+                "missing attribute in configuration: {err}"
+            ) from err
 
     @property
-    def supported_did_regex(self) -> Pattern:
-        """Return supported methods regex."""
-        return self._supported_methods_pattern
+    def supported_methods(self) -> Sequence[str]:
+        """Return supported methods.
+        By determining methods from config file, we preserve the ability to not
+        use the universal resolver for a given method, even if the universal
+        is capable of resolving that method.
+        """
+        return self._supported_methods
 
     async def _resolve(self, _profile: Profile, did: str) -> dict:
         """Resolve DID through remote universal resolver."""
 
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"{self._endpoint}/1.0/identifiers/{did}") as resp:
+            async with session.get(f"{self._endpoint}/{did}") as resp:
                 if resp.status == 200:
                     doc = await resp.json()
                     did_doc = doc["didDocument"]
